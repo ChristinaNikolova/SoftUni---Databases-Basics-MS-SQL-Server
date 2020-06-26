74dbf866d447b55b9bf5e39b2d00bbdc8426f876
ChristinaNikolova <christina.nikolova@abv.bg>
2020-06-26 10:14:20 +0300

    add new solution to task 1

--- DB C# - May 2020/08.Exams/Exam - 21.06.2020/Exam - 21 Jun 2020.sql
+++ DB C# - May 2020/08.Exams/Exam - 21.06.2020/Exam - 21 Jun 2020.sql
@@ -8,15 +8,15 @@ USE TripService;
 CREATE TABLE Cities(
 	Id INT PRIMARY KEY IDENTITY,
 	[Name] NVARCHAR(20) NOT NULL,
-	CountryCode CHAR(2) NOT NULL,
+	CountryCode NCHAR(2) NOT NULL,
 );
 
 CREATE TABLE Hotels(
 	Id INT PRIMARY KEY IDENTITY,
 	[Name] NVARCHAR(30) NOT NULL,
-	CityId INT FOREIGN KEY REFERENCES Cities(Id) NOT NULL,
+	[CityId] INT FOREIGN KEY REFERENCES Cities(Id) NOT NULL,
 	EmployeeCount INT NOT NULL,
-	BaseRate DECIMAL(18, 2),
+	BaseRate DECIMAL(18, 2)
 );
 
 CREATE TABLE Rooms(
@@ -24,19 +24,18 @@ CREATE TABLE Rooms(
 	Price DECIMAL(18, 2) NOT NULL,
 	[Type] NVARCHAR(20) NOT NULL,
 	Beds INT NOT NULL,
-	HotelId INT FOREIGN KEY REFERENCES Hotels(Id) NOT NULL,
+	HotelId INT  FOREIGN KEY REFERENCES Hotels(Id) NOT NULL,
 );
 
-CREATE TABLE Trips
-(
-    Id INT PRIMARY KEY IDENTITY,
-    RoomId INT FOREIGN KEY REFERENCES Rooms(Id) NOT NULL,
-    BookDate DATE NOT NULL,
-    ArrivalDate DATE NOT NULL,
-    ReturnDate DATE NOT NULL,
-    CancelDate DATE,
-    CHECK(BookDate < ArrivalDate),
-    CHECK(ArrivalDate < ReturnDate)
+CREATE TABLE Trips(
+	Id INT PRIMARY KEY IDENTITY,
+	RoomId INT FOREIGN KEY REFERENCES Rooms(Id) NOT NULL,
+	BookDate DATE NOT NULL,
+	ArrivalDate DATE NOT NULL,
+	ReturnDate DATE NOT NULL,
+	CancelDate DATE,
+	CONSTRAINT CH_BookDate CHECK(BookDate < ArrivalDate),
+	CONSTRAINT CH_ArrivalDate CHECK(ArrivalDate < ReturnDate),
 );
 
 CREATE TABLE Accounts(
@@ -46,14 +45,14 @@ CREATE TABLE Accounts(
 	LastName NVARCHAR(50) NOT NULL,
 	CityId INT FOREIGN KEY REFERENCES Cities(Id) NOT NULL,
 	BirthDate DATE NOT NULL,
-	Email VARCHAR(100) NOT NULL UNIQUE,
+	Email NVARCHAR(100) NOT NULL UNIQUE,
 );
 
 CREATE TABLE AccountsTrips(
 	AccountId INT FOREIGN KEY REFERENCES Accounts(Id) NOT NULL,
 	TripId INT FOREIGN KEY REFERENCES Trips(Id) NOT NULL,
 	Luggage INT CHECK(Luggage >= 0) NOT NULL,
-	CONSTRAINT PK_AccountsTrips PRIMARY KEY(AccountId, TripId),
+    CONSTRAINT PK_AccountsTrips PRIMARY KEY(AccountId, TripId),
 );
 
 GO
